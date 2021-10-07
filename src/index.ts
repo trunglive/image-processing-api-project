91e1bbe078b5865bfd6cbaa88ff44d76f3959b58
Trung Vo <trunglive@gmail.com>
2021-10-07 10:40:53 +0700

    feat: setup routes with express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-const myFunc = (num: number): number => {
-  return num * num;
-};
+import express from "express";
+import routes from "./routes";
 
-export default myFunc;
+const app = express();
+const port = 3000;
+
+app.use("/api", routes);
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
+
+// const myFunc = (num: number): number => {
+//   return num * num;
+// };
+
+// export default myFunc;
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const routes = express.Router();
+
+routes.get("/", (req, res) => {
+  res.send("main api route");
+});
+
+export default routes;
