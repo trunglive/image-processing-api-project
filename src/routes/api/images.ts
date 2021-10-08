2a32bb7aa14f20b114c9d691666bd921081d812b
Trung Vo <trunglive@gmail.com>
2021-10-08 15:12:00 +0700

    feat: add initial logic to resize image based on query

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import sharp from "sharp";
 
 const images = express.Router();
 
 images.get("/", (req, res) => {
-  res.send("images route");
+  const { filename } = req.query;
+  const width = Number(req.query.width);
+  const height = Number(req.query.height);
+
+  sharp(`images/${filename}.jpg`)
+    .resize(width, height)
+    .toFile(`images/resize/${filename}-resize.jpg`, (err, info) => {
+      console.log({ err, info });
+    });
+  // res.send("images route");
 });
 
 export default images;
