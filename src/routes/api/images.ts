84f9337d575cf5bcd3911a5f019fbbf99f63e8bf
Trung Vo <trunglive@gmail.com>
2021-10-10 10:05:05 +0700

    feat: serve image after resizing or from cache

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,19 +1,58 @@
 import express from "express";
 import sharp from "sharp";
+import fs from "fs";
 
 const images = express.Router();
 
-images.get("/", (req, res) => {
+images.get("/", async (req, res) => {
   const { filename } = req.query;
   const width = Number(req.query.width);
   const height = Number(req.query.height);
 
-  sharp(`images/${filename}.jpg`)
-    .resize(width, height)
-    .toFile(`images/resize/${filename}-resize.jpg`, (err, info) => {
-      console.log({ err, info });
+  const imageCacheExists = fs.existsSync(
+    `images/resize/${filename}-resize.jpg`
+  );
+
+  try {
+    if (!imageCacheExists) {
+      console.log("resize and serve image");
+
+      const imageBuffer = await fs.readFileSync(`images/${filename}.jpg`);
+      if (!imageBuffer) throw new Error();
+
+      const resizedImage = await sharp(imageBuffer)
+        .resize({ width, height })
+        .jpeg();
+
+      const resizedImageBuffer = await resizedImage.toBuffer();
+
+      res.set("Content-Type", "image/jpeg");
+      res.send(resizedImageBuffer);
+
+      await resizedImage.toFile(
+        `images/resize/${filename}-resize.jpg`,
+        (err, info) => {
+          console.log({ err, info });
+        }
+      );
+    } else {
+      console.log("serve image from cache");
+
+      const imageBuffer = await fs.readFileSync(
+        `images/resize/${filename}-resize.jpg`
+      );
+      if (!imageBuffer) throw new Error();
+
+      res.set("Content-Type", "image/jpeg");
+      res.send(imageBuffer);
+    }
+  } catch (e) {
+    console.log(`Error processing file name ${filename}`);
+
+    res.send({
+      error: `Error processing file name ${filename}`,
     });
-  // res.send("images route");
+  }
 });
 
 export default images;
