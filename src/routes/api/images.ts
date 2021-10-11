ed7f77256e4df4bd9850fa5fe2f25d26627270bb
Trung Vo <trunglive@gmail.com>
2021-10-11 07:47:16 +0700

    style: fix code format in images.ts

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -4,49 +4,46 @@ import fs from 'fs'
 
 const images = express.Router()
 
-images.get(
-  '/',
-  async (req: Request, res: Response): Promise<void> => {
-    const { filename } = req.query
-    const width = Number(req.query.width)
-    const height = Number(req.query.height)
+images.get('/', async (req: Request, res: Response): Promise<void> => {
+  const { filename } = req.query
+  const width = Number(req.query.width)
+  const height = Number(req.query.height)
 
-    const imageCacheExists = fs.existsSync(`images/resize/${filename}-resize.jpg`)
+  const imageCacheExists = fs.existsSync(`images/resize/${filename}-resize.jpg`)
 
-    try {
-      if (!imageCacheExists) {
-        console.log('resize and serve image...')
+  try {
+    if (!imageCacheExists) {
+      console.log('resize and serve image...')
 
-        const imageBuffer = await fs.readFileSync(`images/${filename}.jpg`)
-        if (!imageBuffer) throw new Error()
+      const imageBuffer = await fs.readFileSync(`images/${filename}.jpg`)
+      if (!imageBuffer) throw new Error()
 
-        const resizedImage = await sharp(imageBuffer).resize({ width, height }).jpeg()
+      const resizedImage = await sharp(imageBuffer).resize({ width, height }).jpeg()
 
-        await resizedImage.clone().toFile(`images/resize/${filename}-resize.jpg`, (err, info) => {
-          console.log({ err, info })
-        })
-
-        const resizedImageBuffer = await resizedImage.toBuffer()
-
-        res.set('Content-Type', 'image/jpeg')
-        res.send(resizedImageBuffer)
-      } else {
-        console.log('serve image from cache...')
-
-        const imageBuffer = await fs.readFileSync(`images/resize/${filename}-resize.jpg`)
-        if (!imageBuffer) throw new Error()
-
-        res.set('Content-Type', 'image/jpeg')
-        res.send(imageBuffer)
-      }
-    } catch (err) {
-      console.log(`error processing file name ${filename}`)
-
-      res.send({
-        error: `error processing file name ${filename}`,
+      await resizedImage.clone().toFile(`images/resize/${filename}-resize.jpg`, (err, info) => {
+        console.log({ err, info })
       })
+
+      const resizedImageBuffer = await resizedImage.toBuffer()
+
+      res.set('Content-Type', 'image/jpeg')
+      res.send(resizedImageBuffer)
+    } else {
+      console.log('serve image from cache...')
+
+      const imageBuffer = await fs.readFileSync(`images/resize/${filename}-resize.jpg`)
+      if (!imageBuffer) throw new Error()
+
+      res.set('Content-Type', 'image/jpeg')
+      res.send(imageBuffer)
     }
-  },
-)
+  } catch (err) {
+    console.log(`error processing file name ${filename}`)
+
+    res.send({
+      error: `error processing file name ${filename}`,
+    })
+  }
+})
 
 export default images
