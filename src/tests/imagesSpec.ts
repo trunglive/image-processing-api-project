8b0779dbb46c09e3069e5c3946b99cde757e50f8
Trung Vo <trunglive@gmail.com>
2021-10-17 17:15:15 +0700

    fix: redo jasmine testing function

--- src/tests/imagesSpec.ts
+++ src/tests/imagesSpec.ts
@@ -1,20 +1,31 @@
 import fs from 'fs'
 import supertest from 'supertest'
 import app from '../index'
+import { resizeImage } from '../utils/processImage'
 
 const request = supertest(app)
 
-describe('test endpoint response', () => {
-  it('should resize and save image', async (done) => {
+describe('test image processing api', () => {
+  it('should return success for image processing api', async (done) => {
     const response = await request.get('/api/images?filename=santamonica&width=500&height=300')
+
     expect(response.status).toBe(200)
 
     done()
   })
 
-  it('should load existing image', (done) => {
-    const imageCacheExists = fs.existsSync('images/resize/santamonica-resize.jpg')
-    expect(imageCacheExists).toBe(true)
+  it('should create resized image with correct width/height/format', async (done) => {
+    const imageBuffer = fs.readFileSync(`images/fjord.jpg`)
+
+    const width = 400
+    const height = 300
+    const resizedImage = resizeImage({ imageBuffer, width, height })
+
+    const { info } = await resizedImage.toBuffer({ resolveWithObject: true })
+
+    expect(info.width).toEqual(width)
+    expect(info.height).toEqual(height)
+    expect(info.format).toContain('jpeg')
 
     done()
   })
